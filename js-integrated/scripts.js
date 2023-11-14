193c96ee1b13ae0ffbe26981501b4dd03c64c809
Wither19 <146484561+Wither19@users.noreply.github.com>
2023-11-14 10:09:40 -0500

    Create scripts.js

---
+++ js-integrated/scripts.js
@@ -0,0 +1,9 @@
+const classimg = document.getElementById('classimg');
+const classresidence = document.getElementById('classresidence');
+const health = document.getElementById('health');
+
+function scout() {
+classimg.src ='https://raw.githubusercontent.com/Wither19/thatdivisaspy/main/docs/assets/images/Scout_RED.png'; document.getElementById('meettheclass').innerHTML = 'Meet The Scout'; document.getElementById('classname').innerHTML = 'Real name: Jeremy'; document.getElementById('classage').innerHTML = 'Age: 27';
+classresidence.innerHTML = 'Boston, MA';
+health.innerHTML = '125';
+}
